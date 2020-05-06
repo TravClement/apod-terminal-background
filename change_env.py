20cc8428029c8c06683843c4ae06090c6024a9ee
Zac Clement <>
2020-05-05 19:20:36 -0600

    Working api request

--- change_env.py
+++ change_env.py
@@ -1,4 +1,12 @@
 import requests
+import json
 
-API_KEY = "read from api_key.txt file"
-URL = "http://api.nasa.gov/planetary/apod";
+# Read in the NASA api key from text file
+with open('api_key.txt') as f:
+    API_KEY = f.read()
+URL = "https://api.nasa.gov/planetary/apod"
+# GET request settings to get HD url
+payload = {'hd': 'True', 'api_key': API_KEY}
+
+r = requests.get(URL, params=payload)
+r.json()
