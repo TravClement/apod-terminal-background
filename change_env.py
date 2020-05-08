9fba69b881b542f71ad53546a54b1d0dea274f88
Zac Clement <>
2020-05-08 15:14:02 -0600

    Picture downloaded for the terminal background

--- change_env.py
+++ change_env.py
@@ -9,4 +9,11 @@ URL = "https://api.nasa.gov/planetary/apod"
 payload = {'hd': 'True', 'api_key': API_KEY}
 
 r = requests.get(URL, params=payload)
-r.json()
+request = r.json()
+# Request the hd picture url to save to a file
+pic = requests.get(request['hdurl'])
+# Check that the picture GET request worked
+if pic.status_code == 200:
+    # Open a jpg file to save the picture to
+    with open('apod.jpg', 'wb') as f:
+        f.write(pic.content)
