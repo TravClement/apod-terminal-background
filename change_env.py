65a5edfc9c956f6113b59c326fe121d5537dc344
Zac Clement <>
2020-05-08 16:28:54 -0600

    Changed to use url instead of downloading

--- change_env.py
+++ change_env.py
@@ -1,5 +1,4 @@
-import requests
-import json
+import requests, json, re
 
 # Read in the NASA api key from text file
 with open('api_key.txt') as f:
@@ -7,13 +6,16 @@ with open('api_key.txt') as f:
 URL = "https://api.nasa.gov/planetary/apod"
 # GET request settings to get HD url
 payload = {'hd': 'True', 'api_key': API_KEY}
-
 r = requests.get(URL, params=payload)
 request = r.json()
-# Request the hd picture url to save to a file
-pic = requests.get(request['hdurl'])
-# Check that the picture GET request worked
-if pic.status_code == 200:
-    # Open a jpg file to save the picture to
-    with open('apod.jpg', 'wb') as f:
-        f.write(pic.content)
+
+# Update the settings.json file by reading in the file, then default powershell with the picture
+with open('C:\\Users\\Zac\\AppData\\Local\\Packages\\Microsoft.WindowsTerminal_8wekyb3d8bbwe\\LocalState\\settings.json', 'r') as settings:
+    # load won't work with comments in the file, strip it first to json
+    fixedSettings = re.sub("//\s.*", "", settings.read(), flags=re.MULTILINE)
+    data = json.loads(fixedSettings)
+# Set the default background image to the apod url
+data['profiles']['defaults']['backgroundImage'] = request['hdurl']
+# Reopen the settings file and write the updated image
+with open('C:\\Users\\Zac\\AppData\\Local\\Packages\\Microsoft.WindowsTerminal_8wekyb3d8bbwe\\LocalState\\settings.json', 'w') as settings:
+    json.dump(data, settings)
